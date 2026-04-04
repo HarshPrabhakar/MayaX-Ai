68a092a1db77241ce4e22274ed0334728acafd67
Harsh Prabhakar <harshprabhakar2509@gmail.com>
2026-04-04 05:40:33 +0530

    added codes in app.py

---
+++ backend/app.py
@@ -0,0 +1,43 @@
+from fastapi import FastAPI, File, UploadFile
+from fastapi.middleware.cors import CORSMiddleware
+import torch
+from torchvision import transforms
+from PIL import Image
+import io
+
+app = FastAPI()
+
+# Allow frontend connection
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# Load model
+model = torch.load("model/detector.pt", map_location=torch.device('cpu'))
+model.eval()
+
+# Same transform you wrote
+transform = transforms.Compose([
+    transforms.Resize((224, 224)),
+    transforms.ToTensor(),
+    transforms.Normalize([0.5,0.5,0.5], [0.5,0.5,0.5])
+])
+
+@app.post("/predict")
+async def predict(file: UploadFile = File(...)):
+    image_bytes = await file.read()
+    image = Image.open(io.BytesIO(image_bytes)).convert("RGB")
+    
+    image = transform(image).unsqueeze(0)
+
+    with torch.no_grad():
+        output = model(image)
+        _, predicted = torch.max(output, 1)
+
+    result = "Fake" if predicted.item() == 0 else "Real"
+    
+    return {"prediction": result}
